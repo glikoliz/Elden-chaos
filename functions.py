db9faa5f8a1d2fc502e2678e4be3339df81843d2
glikoliz <r.eirih@ya.ru>
2023-12-14 19:10:06 +0700

    Add new class Funcs, also some new effects

--- functions.py
+++ functions.py
@@ -1,10 +1,51 @@
 import pymem
 from time import sleep
-def OHKO(pm, addr):
-    basehp=pm.read_int(addr)
-    pm.write_int(addr, 1)
+from random import randint
+from linecache import getline
+class Funcs:
+    def disable_fast_travel(pm, final_list):
+        pm.write_bytes(pm.base_address+0x61F232, b'\xBB\x01\x00\x00\x00\x89\x9E\xA0\x00\x00\x00', 11)
+    def enable_fast_travel(pm, final_list): #todo
+        pm.write_bytes(pm.base_address+0x61F232, b'\xBB\x00\x00\x00\x00\x89\x9E\xA0\x00\x00\x00', 11)
+    def warp_to(pm, final_list, grace):
+        pm.write_int(final_list["WARP_LOCATION"], grace)
+        pm.start_thread(final_list["WARP_FUNC"])
+    def wait(pm, final_list):
+        while(pm.read_int(final_list["CUTSCENE_ON"])!=0):
+            sleep(0.1)
+    def respawn_boss(pm, boss_addr):
+        pm.write_uchar(boss_addr, 0)
+        pass
+def print_name(name):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(name)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+@print_name("One Hit KO")
+def OHKO(pm, final_list):
+    basehp=pm.read_int(final_list['MAX_HP'])
+    pm.write_int(final_list['MAX_HP'], 1)
     sleep(10)
-    pm.write_int(addr, basehp)
-    return "OHKO"
-# if __name__=='__main__':
-    # pm=pymem.Pymem('eldenring.exe')
+    pm.write_int(final_list['MAX_HP'], basehp)
+@print_name("Mana leak")
+def MANA_LEAK(pm, final_list):
+    fp=pm.read_int(final_list['FP'])
+    while(fp>=3):
+        fp=pm.read_int(final_list['FP'])-3
+        pm.write_int(final_list['FP'], fp)
+        sleep(0.5)
+@print_name("Teleport to random grace")
+def WARP(pm, final_list):
+    random_number=int(getline("graces.txt", randint(0, 305)).strip())
+    Funcs.warp_to(pm, final_list, random_number)
+@print_name("Rick, Soldier of God")
+def GODRICK_TIME(pm, final_list):
+    Funcs.respawn_boss(pm, final_list["GODRICK"])
+    Funcs.disable_fast_travel(pm, final_list)
+    Funcs.warp_to(pm, final_list, 18002950)
+    while pm.read_uchar(final_list["GODRICK"])==0: #wait until godrick dies
+        sleep(0.5)
+        pass
+    Funcs.enable_fast_travel(pm, final_list)
