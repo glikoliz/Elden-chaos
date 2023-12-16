9bdcce656afec1248dfe9dd55eff717d02d13387
glikoliz <r.eirih@ya.ru>
2023-12-17 04:26:36 +0700

    add function to spawn enemy, that was hard...

--- functions.py
+++ functions.py
@@ -8,14 +8,44 @@ class Funcs:
     def enable_fast_travel(pm, final_list): #todo
         pm.write_bytes(pm.base_address+0x61F232, b'\xBB\x00\x00\x00\x00\x89\x9E\xA0\x00\x00\x00', 11)
     def warp_to(pm, final_list, grace):
+        
         pm.write_int(final_list["WARP_LOCATION"], grace)
         pm.start_thread(final_list["WARP_FUNC"])
+        
     def wait(pm, final_list):
         while(pm.read_int(final_list["CUTSCENE_ON"])!=0):
             sleep(0.1)
     def respawn_boss(pm, boss_addr):
         pm.write_uchar(boss_addr, 0)
         pass
+    def spawn_enemy(pm, final_list, id): #TODO:chr_count may be broken
+        chr_id=("c"+str(id)).encode("utf-16le")
+        pm.write_bytes(final_list["CHR_ID"], chr_id, len(chr_id))
+        pm.write_int(final_list["NPC_PARAM_ID"], id*10000)
+        pm.write_int(final_list["NPC_THINK_PARAM_ID"], id*10000)
+        pm.write_int(final_list["EVENT_ENTITY_ID"], 0)
+        pm.write_int(final_list["TALK_ID"], 0)
+        pm.write_bytes(final_list['NPC_ENEMY_TYPE'], b'\x00', 1)
+        spawned_enemy=final_list["SPAWN_ADDR"].to_bytes(8, byteorder='little')
+        worldchrman=final_list['WORLDCHRMAN'].to_bytes(8, byteorder='little')
+        assembly_code = (
+        b'\x48\xA1'+worldchrman+
+        b'\x48\x8B\x80\x40\xE6\x01\x00'
+        b'\xC6\x40\x44\x01'
+        b'\x8B\x15\x30\x00\x00\x00'
+        b'\x6B\xD2\x10'
+        b'\x48\xBB'+spawned_enemy+
+        b'\x48\x01\xD3'
+        b'\x8A\x80\x78\x01\x00\x00'
+        b'\x88\x43\x0B'
+        b'\xFF\x05\x11\x00\x00\x00'
+        b'\xC3'
+        )
+        l=pm.allocate(100)
+        # print(format(l,'X'))
+        pm.write_bytes(l, assembly_code, len(assembly_code))
+        pm.start_thread(l)
+        pm.free(l)
 def print_name(name):
     def decorator(func):
         def wrapper(*args, **kwargs):
@@ -49,3 +79,20 @@ def GODRICK_TIME(pm, final_list):
         sleep(0.5)
         pass
     Funcs.enable_fast_travel(pm, final_list)
+def spawn_melania(pm, final_list):
+    Funcs.spawn_enemy(pm, final_list,2040)
+    # inj=final_list["SPAWN_ADDR"].to_bytes(8, byteorder='little')
+    # assembly_code = (
+    # b'\x48\xA1\xD8\xCD\x9E\xFE\xF6\x7F\x00\x00'
+    # b'\x48\x8B\x80\x40\xE6\x01\x00'
+    # b'\xC6\x40\x44\x01'
+    # b'\x8B\x15\x30\x00\x00\x00'
+    # b'\x6B\xD2\x10'
+    # b'\x48\xBB'+inj+
+    # b'\x48\x01\xD3'
+    # b'\x8A\x80\x78\x01\x00\x00'
+    # b'\x88\x43\x0B'
+    # b'\xFF\x05\x11\x00\x00\x00'
+    # b'\xC3'
+    # )
+    pass
