0ee26787ab2a1494e445d97691f6996028845ac5
glikoliz <r.eirih@ya.ru>
2023-12-20 00:18:03 +0700

    Now allocate warp func here, also added get_random_function

--- functions.py
+++ functions.py
@@ -1,15 +1,37 @@
 import pymem
 from time import sleep
-from random import randint
+from random import randint, choice
 from linecache import getline
 class Funcs:
     def disable_fast_travel(pm, final_list):
         pm.write_bytes(pm.base_address+0x61F232, b'\xBB\x01\x00\x00\x00\x89\x9E\xA0\x00\x00\x00', 11)
-    def enable_fast_travel(pm, final_list): #todo
+    def enable_fast_travel(pm, final_list): #TODO:can be better
         pm.write_bytes(pm.base_address+0x61F232, b'\xBB\x00\x00\x00\x00\x89\x9E\xA0\x00\x00\x00', 11)
     def warp_to(pm, final_list, grace):
-        pm.write_int(final_list["WARP_LOCATION"], grace)
-        pm.start_thread(final_list["WARP_FUNC"])
+        print("ALLOCATING WARP_FUNC")
+        warp_func=pm.allocate(100)
+        cs_lua_event=final_list['CS_LUA_EVENT'].to_bytes(8, byteorder='little')
+        lua_warp=final_list['LUA_WARP'].to_bytes(8, byteorder='little')
+        bytecode = (
+        b'\x48\x83\xEC\x48'
+        b'\x48\xB8' + cs_lua_event +
+        b'\x48\x8B\x48\x18'
+        b'\x48\x8B\x50\x08' 
+        b'\x8B\x05\x1C\x00\x00\x00'
+        b'\x44\x8D\x80\x18\xFC\xFF\xFF\xFF\x15\x02\x00\x00\x00'
+        b'\xEB\x08' + lua_warp +
+        b'\x48\x83\xC4\x48'
+        b'\xC3' 
+        )
+        warp_loc=warp_func+len(bytecode)
+        # addr=pm.allocate(len(bytecode))
+        pm.write_bytes(warp_func, bytecode, len(bytecode))
+        pm.write_int(warp_loc, grace)
+        print("DO WARP")
+        pm.start_thread(warp_func)
+        print("FREE WARP")
+        
+        pm.free(warp_func)
         
     def wait(pm, final_list):
         while(pm.read_int(final_list["CUTSCENE_ON"])!=0):
@@ -17,9 +39,7 @@ class Funcs:
     def respawn_boss(pm, boss_addr):
         pm.write_uchar(boss_addr, 0)
     def spawn_enemy(pm, final_list, id): #TODO:chr_count may be broken
-        # WRITE CURRENT POS #
-        pm.write_bytes(final_list["SPAWN_NPC_X"], pm.read_bytes(final_list['CURRENT_POS'], 12), 12)
-        # WRITE CURRENT POS #
+        pm.write_bytes(final_list["SPAWN_NPC_X"], pm.read_bytes(final_list['CURRENT_POS'], 12), 12)# WRITE CURRENT POS
         
         # WRITE CHR INFO #
         chr_id=("c"+str(id)).encode("utf-16le")
@@ -51,31 +71,30 @@ class Funcs:
         pm.start_thread(l)
         # print(pm.read_int(l+0x4B))
         pm.free(l)
-def print_name(name):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            print(name)
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
-@print_name("One Hit KO")
+def get_random_function():
+    functions = [
+        (OHKO, "One Hit KO"),
+        (MANA_LEAK, "Mana leak"),
+        (WARP_TO_RANDOM_GRACE, "Teleport to random grace"),
+        (GODRICK_TIME, "Rick, Soldier of God"),
+        (SPAWN_MALENIA, "Spawn Melania")
+    ]
+    random_function, function_name = choice(functions)
+    return random_function, function_name
 def OHKO(pm, final_list):
     basehp=pm.read_int(final_list['MAX_HP'])
     pm.write_int(final_list['MAX_HP'], 1)
     sleep(10)
     pm.write_int(final_list['MAX_HP'], basehp)
-@print_name("Mana leak")
 def MANA_LEAK(pm, final_list):
     fp=pm.read_int(final_list['FP'])
     while(fp>=3):
         fp=pm.read_int(final_list['FP'])-3
         pm.write_int(final_list['FP'], fp)
         sleep(0.5)
-@print_name("Teleport to random grace")
-def WARP(pm, final_list):
+def WARP_TO_RANDOM_GRACE(pm, final_list):
     random_number=int(getline("graces.txt", randint(0, 305)).strip())
     Funcs.warp_to(pm, final_list, random_number)
-@print_name("Rick, Soldier of God")
 def GODRICK_TIME(pm, final_list):
     Funcs.respawn_boss(pm, final_list["GODRICK"])
     Funcs.disable_fast_travel(pm, final_list)
@@ -84,5 +103,5 @@ def GODRICK_TIME(pm, final_list):
         sleep(0.5)
         pass
     Funcs.enable_fast_travel(pm, final_list)
-def spawn_melania(pm, final_list):
+def SPAWN_MALENIA(pm, final_list):
     Funcs.spawn_enemy(pm, final_list, 2120)
