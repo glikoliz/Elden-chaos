12dc3a614ac76987ba83ad207858e5063fa981c8
glikoliz <r.eirih@ya.ru>
2024-02-16 19:57:49 +0700

    Update timer in overlay, now it is graphic, not just timer

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ from PySide6.QtWidgets import (
     QLabel,
     QFrame,
     QMainWindow,
+    QGraphicsView,
+    QGraphicsScene,
+    QGraphicsRectItem,
+    
 )
-from PySide6.QtCore import QRect, Qt, QThread, QTimer, QPropertyAnimation, QUrl
-from PySide6.QtGui import QDesktopServices
+from PySide6.QtCore import QRect, Qt, QThread, QTimer, QPropertyAnimation, QUrl, Signal, Slot
+from PySide6.QtGui import QDesktopServices, QBrush, QPen, QColor
 import threading
 from pymem import Pymem
 
@@ -24,11 +28,12 @@ CHAOS_TIMER_MS = 30000
 
 
 class OverlayController(QThread):
+    updateSignal = Signal(list, int, int)
     def __init__(self, overlay):
         super().__init__()
         self.overlay = overlay
         self.queue = ["", "", ""]
-        self.i = -1
+        self.i = 6
 
     def run(self):
         try:
@@ -44,18 +49,17 @@ class OverlayController(QThread):
         threading.Thread(target=self.start_effect, args=(
             func, name, time, self.i+2)).start()
         self.queue.pop(0)
-        if (time != 0):
-            self.queue.append(f"{name}  {time}s")
-        else:
-            self.queue.append(name)
-        self.overlay.changeText(self.queue)
+        self.queue.append(name)
+        self.updateSignal.emit(self.queue, 70, 2)
         QTimer.singleShot(0, self.overlay.start_animation)
 
     def start_effect(self, effect_class, name, time, ok):
+        import time as tim
         counter = 0
         effect = effect_class()
-        effect.onStart()
+        # effect.onStart()
         funcs=Funcs()
+        start=tim.time()
         while funcs.is_player_in_cutscene():
             sleep(0.5)
         if time == 0:
@@ -64,21 +68,30 @@ class OverlayController(QThread):
                 if (counter >= 120):
                     break
                 sleep(1)
-                
-        for i in range(time):
+        # print(tim.time()-start)
+        i=tim.time()-start
+        while i<29:
+            i=tim.time()-start
             if funcs.is_player_in_cutscene():
                 break
-            effect.onTick()
-            self.queue[ok-self.i] = f"{name}  {time - i}s"
-            self.overlay.changeText(self.queue)
+            # effect.onTick()
+            # self.queue[ok-self.i] = f"{name}  {time - i}s"
+            # print(name, ok-self.i)
+            print(name, i)
+            # self.overlay.changeText(self.queue)
+            self.updateSignal.emit(self.queue, 60-i*2, ok-self.i)
+            # print(f"Time: {tim.time()-start} of {i}")
             sleep(1)
-        self.queue[ok-self.i] = name
-        self.overlay.changeText(self.queue)
-        effect.onStop()
+        self.updateSignal.emit(self.queue, 0, ok-self.i)
+        # self.queue[ok-self.i] = name
+        # self.overlay.changeText(self.queue)
+        # print(tim.time()-start)
+
+        # effect.onStop()
 
     def stop_overlay(self):
         if self.overlay:
-            self.i = -1
+            # self.i = -1
             self.overlay.hide()
             self.overlay.frame.resize(0, 0)
             self.overlay.animation_object.stop()
@@ -87,7 +100,8 @@ class OverlayController(QThread):
 class Overlay(QWidget):
     def __init__(self, overlay_controller):
         super().__init__()
-        # print(self.screen())
+        self.setGeometry(0, 0, 2560, 1440)
+        self.overlay_controller = overlay_controller
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setStyleSheet(
@@ -101,9 +115,17 @@ class Overlay(QWidget):
         """
         )
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.overlay_controller = overlay_controller
+        self.scene = QGraphicsScene(self)
+        self.view = QGraphicsView(self.scene, self)
+        self.view.setHorizontalScrollBarPolicy(
+            Qt.ScrollBarPolicy(Qt.ScrollBarAlwaysOff))
+        self.view.setVerticalScrollBarPolicy(
+            Qt.ScrollBarPolicy(Qt.ScrollBarAlwaysOff))
+        self.view.setFrameStyle(QFrame.NoFrame)
+
+        self.view.setAttribute(Qt.WA_TranslucentBackground)
+        self.view.setStyleSheet("background: transparent;")
+
         self.frame = QFrame(self)
         self.frame.setStyleSheet("background-color: red;")
         self.frame.setFrameStyle(QFrame.Panel | QFrame.Raised)
@@ -111,29 +133,69 @@ class Overlay(QWidget):
         self.frame.resize(self.screen().size().width(),
                           self.screen().size().height()//50)
 
-        self.label1 = QLabel("", self)
-        self.label2 = QLabel("", self)
-        self.label3 = QLabel("", self)
+        self.label1 = QLabel(self)
+        self.label2 = QLabel(self)
+        self.label3 = QLabel(self)
+        self.rects = [
+            self.scene.addRect(0, 0, 70, 0, QPen(Qt.NoPen), QBrush(QColor("green"))), 
+            self.scene.addRect(0, 0, 70, 0, QPen(Qt.NoPen), QBrush(QColor("green"))), 
+            self.scene.addRect(0, 0, 70, 0, QPen(Qt.NoPen), QBrush(QColor("green")))]
+        self.gray_rects = [QGraphicsRectItem(), QGraphicsRectItem(), QGraphicsRectItem()]
+        self.scene.addItem(self.gray_rects[0])
+        self.scene.addItem(self.gray_rects[1])
+        self.scene.addItem(self.gray_rects[2])
+        
+        self.scene.setSceneRect(
+            0, 0, self.screen().size().width(), self.screen().size().height())
+        self.scene.setBackgroundBrush(Qt.transparent)
 
-    def changeText(self, que):
+        # self.worker = Worker()
+        self.overlay_controller.updateSignal.connect(self.changeText)
+
+    @Slot(list, int, int)
+    def changeText(self, que, value, i):
         screen = self.screen().size().width()
         self.label1.setText(que[0])
         self.label2.setText(que[1])
         self.label3.setText(que[2])
+
+        # i=0
         label_width1 = self.label1.fontMetrics().horizontalAdvance(self.label1.text())
         label_width2 = self.label2.fontMetrics().horizontalAdvance(self.label2.text())
         label_width3 = self.label3.fontMetrics().horizontalAdvance(self.label3.text())
+
+        label_height = self.label1.fontMetrics().height()
+        rect_width = 70
+
+        fill_width = value
+        gray_width = rect_width - fill_width
+        if gray_width == rect_width:
+            self.rects[i].setRect(0, 0, 0, 0)
+            self.gray_rects[i].setRect(0, 0, 0, 0)
+            self.label1.setGeometry(
+                screen - label_width1 -
+                20, self.screen().size().height()//50, label_width1, label_height)
+        else:
+            self.rects[i].setRect(
+                screen - rect_width - 10, (i)*40 + self.screen().size().height()//50 + 5, fill_width, label_height - 5)
+            self.gray_rects[i].setBrush(QBrush(QColor("gray")))
+            self.gray_rects[i].setPen(QPen(Qt.NoPen))
+            self.gray_rects[i].setRect(self.rects[i].rect().x(
+            )+fill_width, self.rects[i].rect().y(), gray_width, self.rects[i].rect().height())
+            # print(gray_width)
         self.label1.setGeometry(
-            screen - label_width1, self.frame.y() + 30, label_width1, 40
+            screen - label_width1 - rect_width -
+            20, self.screen().size().height()//50, label_width1, label_height
         )
         self.label2.setGeometry(
-            screen - label_width2, self.label1.y() + 40, label_width2, 40
+            screen - label_width2 - rect_width -
+            20, self.label1.y() + 40, label_width2, label_height
         )
         self.label3.setGeometry(
-            screen - label_width3, self.label2.y() + 40, label_width3, 40
+            screen - label_width3 - rect_width -
+            20, self.label2.y() + 40, label_width3, label_height
         )
-        self.layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
-
+        
     def start_animation(self):
         self.animation_object = QPropertyAnimation(self.frame, b"geometry")
         self.animation_object.setDuration(CHAOS_TIMER_MS)
