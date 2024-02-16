6d740ae433a10bb6cffceb038d7a5cad73a37e00
glikoliz <r.eirih@ya.ru>
2024-02-16 19:58:35 +0700

    Delete self from MessageHandler class

--- gui/messages_gui.py
+++ gui/messages_gui.py
@@ -1,11 +1,11 @@
 from PySide6.QtWidgets import QMessageBox
 class MessageHandler():
-    def show_message(self, message):
+    def show_message(message):
         msg = QMessageBox()
         msg.setText(message)
         msg.setWindowTitle("Message")
         msg.exec()
-    def show_error_message(self, error_message):
+    def show_error_message(error_message):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Critical)
         msg.setText(error_message)
